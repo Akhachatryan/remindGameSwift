615412fcd6a9c96701331f58a0de122ff3e8337b
Ashot <macashot199514@gmail.com>
2019-04-04 12:46:38 +0400

    Initial Commit

---
+++ RemindGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RemindGame
+//
+//  Created by Ashot on 4/4/19.
+//  Copyright © 2019 Ashot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
